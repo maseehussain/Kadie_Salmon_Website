a452f0d7d80992d76c76665b8d1aac28d1aeb245
Ma5ee2 <hussainmasee@gmail.com>
2018-03-27 16:21:06 +0100

    made and exported art work schema

--- models/artWork.js
+++ models/artWork.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose');
 
-const artWorkSchema
+const artWorkSchema = mongoose.Schema({
+  image: { type: String },
+  nameOfWork: { type: String },
+  yearCreated: { type: String },
+  about: { type: String }
+});
+
+module.exports = mongoose.model('Artwork', artWorkSchema);
