8a1dc1abb4f38d5ffdb8960451417ff93e99a6d4
Ma5ee2 <hussainmasee@gmail.com>
2018-03-29 18:38:17 +0100

    made changes to the models artwork file, changed specs

--- models/artWork.js
+++ models/artWork.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const artWorkSchema = mongoose.Schema({
   image: { type: String },
   nameOfWork: { type: String },
-  yearCreated: { type: String },
-  about: { type: String }
+  material: { type: String },
+  yearCreated: { type: String }
 });
 
 module.exports = mongoose.model('Artwork', artWorkSchema);
