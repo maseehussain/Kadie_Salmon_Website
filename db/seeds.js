bfe682740a0f30928f65eb22725ca2693cad1485
Ma5ee2 <hussainmasee@gmail.com>
2018-03-30 16:50:30 +0100

    re-structured the seed file

--- db/seeds.js
+++ db/seeds.js
@@ -5,11 +5,20 @@ const { dbURI } = require('../config/environment');
 const ArtWork = require('../models/artWork');
 
 mongoose.connect(dbURI, { useMongoCLient: true });
-mongoose.connection.db.dropDatabase();
 
-const artWorkData = [{
-  image: 'https://static.wixstatic.com/media/5b517d_10820e2e44c54888a7193a3416a22540~mv2.jpg/v1/fill/w_801,h_1132,al_c,q_90,usm_0.66_1.00_0.01/5b517d_10820e2e44c54888a7193a3416a22540~mv2.webp',
-  nameOfWork: 'Blue Grey(Sula) ',
-  material: 'Hand painted black and white photograph',
-  yearCreated: '2016'
-}];
+mongoose.connection.once('open', () => {
+  mongoose.connection.db.dropDatabase();
+
+  const artWorksData = [{
+    image: 'https://static.wixstatic.com/media/5b517d_10820e2e44c54888a7193a3416a22540~mv2.jpg/v1/fill/w_801,h_1132,al_c,q_90,usm_0.66_1.00_0.01/5b517d_10820e2e44c54888a7193a3416a22540~mv2.webp',
+    nameOfWork: 'Blue Grey(Sula) ',
+    material: 'Hand painted black and white photograph',
+    yearCreated: '2016'
+  }];
+
+});
+
+
+ArtWork
+  .create(artWorkData)
+  .then
