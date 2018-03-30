a060107076449733d7a640e6b05b25eb910ae47d
Ma5ee2 <hussainmasee@gmail.com>
2018-03-30 16:37:10 +0100

    entered the details of the art work to test on insomnia

--- db/seeds.js
+++ db/seeds.js
@@ -10,10 +10,10 @@ mongoose
   .then(() => {
     return ArtWork.create([
       {
-        image: '',
-        nameOfWork: '',
-        material: '',
-        yearCreated: ''
+        image: 'https://static.wixstatic.com/media/5b517d_10820e2e44c54888a7193a3416a22540~mv2.jpg/v1/fill/w_801,h_1132,al_c,q_90,usm_0.66_1.00_0.01/5b517d_10820e2e44c54888a7193a3416a22540~mv2.webp',
+        nameOfWork: 'Blue Grey(Sula) ',
+        material: 'Hand painted black and white photograph',
+        yearCreated: '2016'
       },
     ])
   })
