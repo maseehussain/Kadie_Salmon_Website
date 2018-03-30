8faf0d72582113109be2e6bb5f75b6c861aa4d6e
Ma5ee2 <hussainmasee@gmail.com>
2018-03-30 16:44:56 +0100

    data did not seed changed the layout of seed.js

--- db/seeds.js
+++ db/seeds.js
@@ -4,19 +4,12 @@ mongoose.Promise = require('bluebird');
 const { dbURI } = require('../config/environment');
 const ArtWork = require('../models/artWork');
 
-mongoose
-  .connect(dbURI, { useMongoCLient: true })
-  .then(db => db.dropDatabase())
-  .then(() => {
-    return ArtWork.create([
-      {
-        image: 'https://static.wixstatic.com/media/5b517d_10820e2e44c54888a7193a3416a22540~mv2.jpg/v1/fill/w_801,h_1132,al_c,q_90,usm_0.66_1.00_0.01/5b517d_10820e2e44c54888a7193a3416a22540~mv2.webp',
-        nameOfWork: 'Blue Grey(Sula) ',
-        material: 'Hand painted black and white photograph',
-        yearCreated: '2016'
-      },
-    ])
-  })
-  .then(artworks => console.log(`${artworks.length} art work created`))
-  .catch(err => console.log(err))
-  .finally(() => mongoose.connection.close());
+mongoose.connect(dbURI, { useMongoCLient: true });
+mongoose.connection.db.dropDatabase();
+
+const artWorkData = [{
+  image: 'https://static.wixstatic.com/media/5b517d_10820e2e44c54888a7193a3416a22540~mv2.jpg/v1/fill/w_801,h_1132,al_c,q_90,usm_0.66_1.00_0.01/5b517d_10820e2e44c54888a7193a3416a22540~mv2.webp',
+  nameOfWork: 'Blue Grey(Sula) ',
+  material: 'Hand painted black and white photograph',
+  yearCreated: '2016'
+}];
