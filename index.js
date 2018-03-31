203d41269d707cb107b1161b5f01cb7d5d0ae749
Ma5ee2 <hussainmasee@gmail.com>
2018-03-31 15:13:06 +0100

    tested the seeded data through insomnia and removed web-sockets

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-// const sockets = require('./lib/sockets');
-// sockets.connect(server);
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
