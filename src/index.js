cbfe9f2453da83175d8c09ee4927158d51da2eb3
Ma5ee2 <hussainmasee@gmail.com>
2018-04-02 20:18:01 +0100

    imported bootstrap css only

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import 'bootstrap-css-only';
+
 class App extends React.Component {
 
   render() {
     return (
       <main>
         <div>
-          <h1></h1>
+          <div>
+
+          </div>
         </div>
       </main>
     );
