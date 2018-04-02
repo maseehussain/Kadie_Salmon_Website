438388fcca2215ec0539453b3e28d34739010727
Ma5ee2 <hussainmasee@gmail.com>
2018-04-02 20:16:09 +0100

    created html boiler point and also imported React to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <main>
+        <div>
+          <h1></h1>
+        </div>
+      </main>
+    );
+  }
+}
