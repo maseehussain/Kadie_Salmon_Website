8782f61adf4d31acd422a71bf9bd4ce152049086
Ma5ee2 <hussainmasee@gmail.com>
2018-04-02 20:11:32 +0100

    created html boiler point and also imported React to index.js

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 4000;
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/kadie_salmon_${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/kadie_salmon_website${env}`;
 const secret = process.env.AUTH_SECRET || 'hYgs^=?>@qrTfxLp';
 
 module.exports = { env, port, dbURI, secret };
